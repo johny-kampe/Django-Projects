1e7ff4adfa94109474b01418689d51776d095773
John Kamperakis <johnkamperakis@gmail.com>
2022-11-24 02:32:38 +0200

    Update Data Part 1

--- Django Practise/My_First_Project/first_app/urls.py
+++ Django Practise/My_First_Project/first_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('album_form/', views.album_form, name='album_form'),
     path('musician_form/', views.musician_form, name='musician_form'),
     path('album_list/<int:artist_id>', views.album_list, name='album_list'),
+    path('edit_artist/<int:artist_id>', views.edit_artist, name='edit_artist'),
 ]
